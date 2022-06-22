829146b29f24a6c5b28c3a5c7b9e11eec45730d0
wanda prasetya <107533381+wandaprasetya23@users.noreply.github.com>
2022-06-22 19:02:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,54 @@
+/**
+   * Create By Wanda Prasetya
+   * Recode By Wanda
+   * Contact Me on wa.me/628975217200
+   * subscribe : https://m.youtube.com/wanda prasetya
+*/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '6fb0eff124',
+}
+
+// Other
+global.owner = ['628975217200']
+global.premium = ['628975217200']
+global.packname = 'wanda prasetya'
+global.author = 'wanda'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '⭔'
+global.mess = {
+    success: '✓ Success',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 100
+}
+global.thumb = fs.readFileSync('./lib/arietube.jpg')
+global.vitube = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
 
